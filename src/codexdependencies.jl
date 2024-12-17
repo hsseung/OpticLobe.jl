e370c704a20d88844573b0972ad51a729283586b
Sebastian Seung <sseung@princeton.edu>
2024-12-17 09:22:35 -0500

    * corrected GCS download path, switched to MD5

--- src/codexdependencies.jl
+++ src/codexdependencies.jl
@@ -1,6 +1,7 @@
 using DataDeps
+using MD5    # switching as GCS provides human-readable MD5 checksums. DataDeps default is SHA256
 
-DOWNLOADS = "https://storage.googleapis.com/flywire-data/codex/data/783"
+DOWNLOADS = "https://storage.googleapis.com/flywire-data/codex/data/fafb/783"
 
 # this has never changed, so specifying version seems unnecessary
 register(DataDep("Codex cell stats",
@@ -12,9 +13,9 @@ register(DataDep("Codex cell stats",
 
     Citation: Matsliah et al.
     """,
-    joinpath(DOWNLOADS, "cell_stats.csv.gz?generation=1709735415919143"),
-    "bd5879e1b5df964bea2f3ca5316348d4276ce2ccaac283f0e36583c04fbd3d8e"
-    ));
+    joinpath(DOWNLOADS, "cell_stats.csv.gz"),
+                 md5("a79d4875e2b80e51d1269dcb86dd8743"),
+));
 
 
 register(DataDep("Codex neuron IDs",
@@ -27,7 +28,7 @@ register(DataDep("Codex neuron IDs",
     Citation: Matsliah et al.
     """,
     joinpath(DOWNLOADS, "neurons.csv.gz"),
-    "6a6b3759e635f0f35a677d169052362131ec61d95f55919298b55c43fce4e719"
+                 md5("f60333e9e4124160b9b203b1712a6f91"),
 ));
 
 register(DataDep("Codex visual neuron types",
@@ -40,7 +41,7 @@ register(DataDep("Codex visual neuron types",
     Citation: Matsliah et al.
     """,
     joinpath(DOWNLOADS, "visual_neuron_types.csv.gz?generation=1720619193993712"),
-    "4d6aee4af9f0aed3098a4eb91163508a61ded6363a3c441a7d50a5f6fcf5c366"
+                 md5("f1870fc7c2bbfe7b99b869fdb83cf7ff"),
 ));
 
 register(DataDep("Codex classification",
@@ -52,8 +53,8 @@ register(DataDep("Codex classification",
 
     Citation: Matsliah et al.
     """,
-    joinpath(DOWNLOADS, "classification.csv.gz?generation=1721489294315289"),
-    "cfd265c5b650df0caaf8b6d02386c9ec53f3c0029102430cf2763d5b40ee6f7d"
+    joinpath(DOWNLOADS, "classification.csv.gz?generation=1733428415941889"),
+                 md5("6200e3f2016e113a829c92ecdeb4ac9c"),
 ));
 
 register(DataDep("Codex connections no threshold",
@@ -63,10 +64,10 @@ register(DataDep("Codex connections no threshold",
     License:
     Website: codex.flywire.ai
 
-    Citation: Matsliah et al.
+        Citation: Matsliah et al.
 
     This file is 202MB
     """,
     joinpath(DOWNLOADS, "connections_no_threshold.csv.gz"),
-    "e40e028b47f9bfc9b6b5a8da534be307eaded1f716b9a39e2c4a586c9f7fc67e"
-));
+                 md5("8332c0f03acd2419f56f9bbc002a1554"),
+    ));
